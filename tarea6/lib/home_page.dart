0c7bcb4a3b645beff3bbd18de271e047d33541cc
Erick Gamboa Valle <is710115@iteso.mx>
2021-09-28 19:00:13 -0500

    Tarea 6 commit to git

---
+++ tarea6/lib/home_page.dart
@@ -0,0 +1,141 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, avoid_unnecessary_containers
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:tarea6/counter.dart';
+import 'package:tarea6/theme.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Provider.of<themeChanger>(context);
+    final Color? color = Colors.grey[200];
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pink[200],
+        title: Text("Contador v2.0"),
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.70,
+            margin: EdgeInsets.all(14),
+            decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Center(
+              child: Consumer<Counter>(
+                builder: (context, counter, child) {
+                  return Text("${counter.count}",
+                      style: TextStyle(fontSize: 72, color: Colors.white));
+                },
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MaterialButton(
+                child: Text(
+                  "BLACK",
+                  style: TextStyle(color: Colors.grey[200]),
+                ),
+                color: Colors.black87,
+                onPressed: () => theme.setTheme(ThemeData(
+                  brightness: Brightness.light,
+                  primaryColor: Colors.black,
+                )),
+              ),
+              MaterialButton(
+                child: Text(
+                  "RED",
+                  style: TextStyle(color: Colors.grey[200]),
+                ),
+                color: Colors.red,
+                onPressed: () => theme.setTheme(
+                  ThemeData(
+                    brightness: Brightness.light,
+                    primaryColor: Colors.red,
+                  ),
+                ),
+              ),
+              MaterialButton(
+                child: Text(
+                  "BLUE",
+                  style: TextStyle(color: Colors.grey[200]),
+                ),
+                color: Colors.blue,
+                onPressed: () => theme.setTheme(
+                  ThemeData(
+                    brightness: Brightness.light,
+                    primaryColor: Colors.blue,
+                  ),
+                ),
+              ),
+              MaterialButton(
+                child: Text(
+                  "GREEN",
+                  style: TextStyle(color: Colors.grey[200]),
+                ),
+                color: Colors.green,
+                onPressed: () => theme.setTheme(
+                  ThemeData(
+                    brightness: Brightness.light,
+                    primaryColor: Colors.green,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              CircleAvatar(
+                maxRadius: 30,
+                minRadius: 20,
+                backgroundColor: Colors.pink[200],
+                child: IconButton(
+                  tooltip: "Sumar 1 cuenta",
+                  icon: Icon(Icons.add),
+                  color: Colors.grey[200],
+                  onPressed: () {
+                    context.read<Counter>().increment();
+                  },
+                ),
+              ),
+              CircleAvatar(
+                maxRadius: 30,
+                minRadius: 20,
+                backgroundColor: Colors.pink[200],
+                child: IconButton(
+                  tooltip: "Restar 1 cuenta",
+                  icon: Icon(Icons.remove),
+                  color: Colors.grey[200],
+                  onPressed: () {
+                    context.read<Counter>().decrement();
+                  },
+                ),
+              ),
+              CircleAvatar(
+                maxRadius: 30,
+                minRadius: 20,
+                backgroundColor: Colors.pink[200],
+                child: IconButton(
+                  tooltip: "Reiniciar cuenta",
+                  icon: Icon(Icons.restart_alt),
+                  color: Colors.grey[200],
+                  onPressed: () {
+                    context.read<Counter>().restart();
+                  },
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
